ebf4fb0082862ed48eda35427a55bd217c4765c1
ggfcoffee <364598566@qq.com>
2016-11-21 14:08:29 +0800

    测试内容提交

--- src/BLL/AccountService.cs
+++ src/BLL/AccountService.cs
@@ -12,19 +12,20 @@ using System.Security.Claims;
 using System.Security.Authentication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Authentication;
+
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BLL
 {
     public class AccountService : IAccountService
     {
-        IUserRepository UserRepository;
+        public IUserRepository UserRepository;
 
         public AccountService(IUserRepository userRepository)
         {
             UserRepository = userRepository;
 
-            
+            //这里是测试的内容提交  
         }
 
         public async Task<string> GetAccountName()
